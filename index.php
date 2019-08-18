a907d679ff65c7d0f5ed1d32dd4fd8708a42fa16
lieoner <popcaric@gmail.com>
2019-08-19 05:10:53 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Getting Started</title>
+  <script src="dist/js/bundle.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="h4 text-center my-5">Bootstrap 4 (пример)</h1>
+  </div>
+</body>
+
+</html>
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css';
+
+(function($) {
+  $(document).ready(function() {});
+})(jQuery);
