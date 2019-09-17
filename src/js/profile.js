28eba16e3ece263670ec35564fef1a93d50366f6
lieoner <popcaric@gmail.com>
2019-09-17 16:25:37 +0700

    17.09.2019
    ПЕРЕКЛЮЧЕНИЕ ВКЛАДОК В ПРОФИЛЕ

--- src/js/profile.js
+++ src/js/profile.js
@@ -3,7 +3,13 @@
         $('.list-group .list-group-item-action').on('click', function(e) {
             e.preventDefault();
             $('.list-group .list-group-item-action.active').removeClass('active');
+            $('.item-data.active').removeClass('active');
             $(this).addClass('active');
+            var data_tab = $(
+                '.item-data[data-val=' +
+                    $('.list-group .list-group-item-action.active').attr('data-val') +
+                    ']'
+            ).addClass('active');
         });
     });
 })(jQuery);
--- src/php/profile/body.php
+++ src/php/profile/body.php
@@ -6,14 +6,15 @@
           <div class="list-group-item label">
             Личный кабинет
           </div>
-          <a href="#" class="list-group-item list-group-item-action active">Профиль
+          <a href="#" class="list-group-item list-group-item-action active" data-val="1">Профиль
           </a>
-          <a href="#" class="list-group-item list-group-item-action">Ещечто-то
+          <a href="#" class="list-group-item list-group-item-action" data-val="2">Ещечто-то
           </a>
         </div>
       </div>
       <div class="col-9">
-
+        <div class="item-data active" data-val="1">sdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasdsdasdasd</div>
+        <div class="item-data" data-val="2">ggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsgggasgsg</div>
       </div>
     </div>
   </div>
