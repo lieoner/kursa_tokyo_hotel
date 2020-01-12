c0014a4c91f2a8d084e7be2343700cd97b043389
lieoner <popcaric@gmail.com>
2020-01-12 21:09:43 +0700

    оплата и начало графиков

--- src/php/admin/body.php
+++ src/php/admin/body.php
@@ -28,24 +28,7 @@
     <div class="col-12 text-center is_auth">
       <legend>Вы авторизированы</legend>
     </div>
-    <!-- 
-    <div id="find-book-form" class="col-12">
-      <form>
-        <fieldset>
-          <div class="row justify-content-center">
-            <label for="book-number" class="col-2 col-form-label text-right">Номер брони:</label>
-            <div class="col-4">
-              <input type="text" class="form-control col-12" id="book-number" name="book-number" placeholder="000 000 000">
-              <div class="invalid-feedback">Слишком не длинный номер.</div>
-            </div>
-            <div class="col-2">
-              <button type="submit" class="btn btn-primary">Найти</button>
-            </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
- -->
+
     <div class='admin-buttons col-12'>
       <div class="row">
         <div class="col-12 text-center register">
@@ -79,7 +62,7 @@
           <button type="button" class="btn btn-secondary btn-sm col-1 book-table-refresh">↻</button>
         </div>
 
-        <div class="book-confirmed alert alert-dismissible alert-success">
+        <div class="col-12 book-confirmed alert alert-dismissible alert-success">
           <strong>Заселение подтверждено</strong>
         </div>
         <table class="table table-hover book-table">
@@ -130,7 +113,7 @@
                   <a class="dropdown-item" href="#" data-operation-id=<?= 'all' ?>>Все</a>
                   <?
                   foreach ($operations as $operation) {
-                    ?>
+                  ?>
                     <a class="dropdown-item" href="#" data-operation-id=<?= $operation['IDop'] ?>><?= $operation['opName'] ?></a>
                   <?
                   } ?>
@@ -149,6 +132,7 @@
     </div>
 
     <div class="col-12 service-content">
+
       <div class="service-roomNumber" style="margin-top:20px;">
         <div class="row justify-content-center">
           <div id="find-service-table" class="col-8 row">
@@ -201,7 +185,73 @@
       </div>
 
     </div>
-  </div>
 
-</div>
+    <div class="col-12 payment-content">
+      <div class="row justify-content-center">
+
+        <label for="table-payment-number" class="col-2 col-form-label text-right">Номер брони:</label>
+        <div id="find-payment-table" class="col-8 row">
+          <input type="text" class="form-control col-10 book-number" id="table-payment-number" name="payment-number" placeholder="000 000 000" autofocus autocomplete="off">
+          <button type="button" class="btn btn-secondary btn-sm col-1 payment-table-refresh">↻</button>
+        </div>
+
+
+        <div class="col-12 payment-confirmed alert alert-dismissible alert-success">
+          <strong>Оплата подтверждена</strong>
+        </div>
+
+        <table class="table table-hover payment-table">
+          <thead>
+            <tr>
+              <th scope="col">Номер брони</th>
+              <th scope="col">Номер</th>
+              <th scope="col">Кол-во дней</th>
+              <th scope="col">Услуги</th>
+              <th scope="col">Проживание</th>
+              <th scope="col">Итого к оплате</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+
+        <div class="modal">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Оплата</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Подтвердить</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="col-12 statistic-content">
+      <div class="row justify-content-center">
+        <div class="btn-group-vertical graphtype col-12" data-toggle="buttons">
+          <button type="button" class="btn btn-outline-primary btn-lg top-service-btn">Самая популярная услуга</button>
+          <button type="button" class="btn btn-outline-primary btn-lg profit-btn">Доход за период</button>
+          <button type="button" class="btn btn-outline-primary btn-lg top-number-btn">Самый популярный класс номеров</button>
+        </div>
+        <div class="jumbotron">
+          <div class="col-12"><button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button></div>
+          <div class="graph col-12"><canvas id="chart" width="1000" height="600"></canvas></div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
 </div>
--- src/php/header.php
+++ src/php/header.php
@@ -6,7 +6,6 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <title>tokyohotel</title>
-
   <?php
   require_once('connection.php');
   require_once('core.php');
